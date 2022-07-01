a69525a2788092f677619bd7d838268d1d588abb
Alex Pence <alexpence719@gmail.com>
2022-07-01 19:15:05 -0400

    box-generator

---
+++ react/box-generator/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { useState } from 'react';
+
+
+import Form from './components/Form';
+import List from './components/List';
+
+function App() {
+  const [boxArray, setBoxArray] = useState([]);
+  return <div className="App">
+      <Form boxArray = {boxArray} setBoxArray = {setBoxArray} />
+      <List boxArray = {boxArray} />
+    </div>;
+}
+
+export default App;
